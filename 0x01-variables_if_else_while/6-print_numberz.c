0fbe2b320007aea2beaee0e386708cc07b7aff1e
Anugo1 <ekehumphery@gmail.com>
2022-06-05 19:08:33 -0700

    6-print_numberz.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,17 +9,10 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
-	return (0);
+for (n  = 0; n  <= 9  ; n++)
+putchar(n + '0');
+putchar('\n');
+return (0);
 }
