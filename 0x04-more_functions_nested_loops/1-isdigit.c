7142a0fd38c4e58ddfef53294a28af0c375e98e1
Anugo1 <ekehumphery@gmail.com>
2022-06-09 18:44:39 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,23 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isdigit - A function that checks for a digit (0 through 9).
- * @c: An input integer
- * Return: 1 if c is a digit or 0 otherwise
- */
+ *  * _isdigit - check for a digit (0 through 9)
+ *   * @c: digit checked
+ *    * Return: 1 if c is a digit returns 0 otherwise
+*/
+
 int _isdigit(int c)
 {
-	char i = '0';
-	int isdigit = 0;
-
-	for (; i <= '9'; i++)
-	{
-		if (i == c)
-		{
-			isdigit = 1;
-			break;
-		}
-	}
-
-	return (isdigit);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
