5e0acc22326b140c4c494bb538192fae7649be42
Anugo1 <ekehumphery@gmail.com>
2022-06-13 17:43:19 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset to 98
+ *
+ * @n: enters integer
+ *
+ * Return: Always 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers and typrd of arrays in c
